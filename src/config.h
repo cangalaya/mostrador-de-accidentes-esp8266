4b548cae17c18a4dee1efc5d42d9f55ddf706b65
alonso cangalaya <cangalaya.2007@gmail.com>
2022-02-02 21:21:49 -0500

    first version done

--- src/config.h
+++ src/config.h
@@ -4,9 +4,9 @@ IPAddress subnet(255, 255, 255, 0);
 
 // ============ CONFIGURACIÓN DEL EQUIPO ===========
 // Configuración Tarjeta
-String cliente = "tasa";
+String cliente = "komatsu";
 String sede = "callao";
-String nombre_ambiente = "comedor2";
+String nombre_ambiente = "taller";
 unsigned int aforo_init = 28;
 unsigned int inactivity_hours_reset = 2;
 unsigned int count_dalay_milisegundos = 100;
@@ -15,8 +15,8 @@ unsigned int logger_interval_minutes = 60;
 String abcd = "medium";
 String estado = "on";
 // Configuración Wifi
-String ssid = "WF_P2_2";         // WF_AFORO
-String password = "R420437015R"; // ss1d_4FoRo_T4s4
+String ssid = "iPhone 11";         // WF_AFORO
+String password = "manuel2007"; // ss1d_4FoRo_T4s4
 
 // Configuración UDP
 unsigned int master_port = 8890;
@@ -96,7 +96,7 @@ void trataDeDatoString(String section, String item, int direccionMemoria)
     if (datoLeidoMemoria != datoLeidoRTBD) // verificar si el dato es el mismo guardado en memoria
     {
       writeStringToEEPROM(direccionMemoria, String(datoLeidoRTBD));
-      Serial.println("   - " + item + " nuevo en eeprom " + readStringFromEEPROM(direccionMemoria));
+      Serial.println("   - " + item + " nuevo en eeprom " + readStringFromEEPROM(direccionMemoria) + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< EEPROM");
     }
   }
   else
@@ -150,113 +150,113 @@ void setCofigEprom()
   {
     Serial.println();
     Serial.println("..OK FIREBASE  CONECTADO");
-    Serial.println(" WIFI :");
-    trataDeDatoString("wifi", "ssid", 0);      //
-    trataDeDatoString("wifi", "password", 50); //
+    // Serial.println(" WIFI :");
+    // trataDeDatoString("wifi", "ssid", 0);      //
+    // trataDeDatoString("wifi", "password", 50); //
 
-    Serial.println(" UDP :");
-    trataDeDatoInt("udp", "master-port", 150); // OK
-    trataDeDatoInt("udp", "second-port", 160); // OK
+    // Serial.println(" UDP :");
+    // trataDeDatoInt("udp", "master-port", 150); // OK
+    // trataDeDatoInt("udp", "second-port", 160); // OK
 
     Serial.println(" TARJET :");
-    trataDeDatoInt("tarjet", "aforo", 190);                    // OK
-    trataDeDatoString("tarjet", "abcd", 200);                  // OK
-    trataDeDatoInt("tarjet", "count-delay-milisegundos", 210); // OK
-    trataDeDatoString("tarjet", "estado", 220);
-    trataDeDatoFloat("tarjet", "inactivity-hours-reset", 230);   // OK flotante
-    trataDeDatoInt("tarjet", "set-data-realtime-segundos", 240); // OK
-    trataDeDatoInt("tarjet","logger-interval-minutes", 250 );
+    trataDeDatoInt("tarjet", "accidentes", 0);                    // OK
+    // trataDeDatoString("tarjet", "abcd", 200);                  // OK
+    // trataDeDatoInt("tarjet", "count-delay-milisegundos", 210); // OK
+    // trataDeDatoString("tarjet", "estado", 220);
+    // trataDeDatoFloat("tarjet", "inactivity-hours-reset", 230);   // OK flotante
+    // trataDeDatoInt("tarjet", "set-data-realtime-segundos", 240); // OK
+    // trataDeDatoInt("tarjet","logger-interval-minutes", 250 );
   }
-  else
-  {
+  // else
+  // {
 
-    Serial.println(">>>> !!! Firebase no conectado -> estableciendo valores por defecto o usando ya escritos");
+  //   Serial.println(">>>> !!! Firebase no conectado -> estableciendo valores por defecto o usando ya escritos");
 
-    // WIFI
-    if (EEPROM.read(0) == 255)
-      writeStringToEEPROM(0, ssid); // si la eeprom en la dirección 0 esta vaćia, escribe el valor por defecto
-    if (EEPROM.read(50) == 255)
-      writeStringToEEPROM(50, password);
+  //   // WIFI
+  //   if (EEPROM.read(0) == 255)
+  //     writeStringToEEPROM(0, ssid); // si la eeprom en la dirección 0 esta vaćia, escribe el valor por defecto
+  //   if (EEPROM.read(50) == 255)
+  //     writeStringToEEPROM(50, password);
 
-    Serial.println("- WIFI:");
-    Serial.println("   - ssid = " + readStringFromEEPROM(0));
-    Serial.println("   - password = " + readStringFromEEPROM(50));
+  //   Serial.println("- WIFI:");
+  //   Serial.println("   - ssid = " + readStringFromEEPROM(0));
+  //   Serial.println("   - password = " + readStringFromEEPROM(50));
 
-    // UDP
-    if (EEPROM.read(150) == 255)
-      writeStringToEEPROM(150, String(master_port)); // ocupa 10 espacios
-    if (EEPROM.read(160) == 255)
-      writeStringToEEPROM(160, String(second_port));
+  //   // UDP
+  //   if (EEPROM.read(150) == 255)
+  //     writeStringToEEPROM(150, String(master_port)); // ocupa 10 espacios
+  //   if (EEPROM.read(160) == 255)
+  //     writeStringToEEPROM(160, String(second_port));
 
-    Serial.println("- UDP:");
-    Serial.println("   - master port = " + readStringFromEEPROM(150));
-    Serial.println("   - second port = " + readStringFromEEPROM(160));
+  //   Serial.println("- UDP:");
+  //   Serial.println("   - master port = " + readStringFromEEPROM(150));
+  //   Serial.println("   - second port = " + readStringFromEEPROM(160));
 
-    // TARJET
-    if (EEPROM.read(190) == 255)
-      writeStringToEEPROM(190, String(aforo_init));
-    if (EEPROM.read(200) == 255)
-      writeStringToEEPROM(200, abcd);
-    if (EEPROM.read(210) == 255)
-      writeStringToEEPROM(210, String(count_dalay_milisegundos));
-    if (EEPROM.read(220) == 255)
-      writeStringToEEPROM(220, estado);
-    if (EEPROM.read(230) == 255)
-      writeStringToEEPROM(230, String(inactivity_hours_reset));
-    if (EEPROM.read(240) == 255)
-      writeStringToEEPROM(240, String(set_data_realtime_segundos));
-    if (EEPROM.read(250) == 255)
-      writeStringToEEPROM(250, String(logger_interval_minutes));
+  //   // TARJET
+  //   if (EEPROM.read(190) == 255)
+  //     writeStringToEEPROM(190, String(aforo_init));
+  //   if (EEPROM.read(200) == 255)
+  //     writeStringToEEPROM(200, abcd);
+  //   if (EEPROM.read(210) == 255)
+  //     writeStringToEEPROM(210, String(count_dalay_milisegundos));
+  //   if (EEPROM.read(220) == 255)
+  //     writeStringToEEPROM(220, estado);
+  //   if (EEPROM.read(230) == 255)
+  //     writeStringToEEPROM(230, String(inactivity_hours_reset));
+  //   if (EEPROM.read(240) == 255)
+  //     writeStringToEEPROM(240, String(set_data_realtime_segundos));
+  //   if (EEPROM.read(250) == 255)
+  //     writeStringToEEPROM(250, String(logger_interval_minutes));
 
-    Serial.println("- TARJET:");
-    Serial.println("   - aforo init = " + readStringFromEEPROM(190));
-    Serial.println("   - abcd = " + readStringFromEEPROM(200));
-    Serial.println("   - count dalay milisegundos = " + readStringFromEEPROM(210));
-    Serial.println("   - estado = " + readStringFromEEPROM(220));
-    Serial.println("   - inactivity hours reset = " + readStringFromEEPROM(230));
-    Serial.println("   - set data realtime segundos = " + readStringFromEEPROM(240));
-    Serial.println("   - logger interval minutes = " + readStringFromEEPROM(250));
-  }
+  //   Serial.println("- TARJET:");
+  //   Serial.println("   - aforo init = " + readStringFromEEPROM(190));
+  //   Serial.println("   - abcd = " + readStringFromEEPROM(200));
+  //   Serial.println("   - count dalay milisegundos = " + readStringFromEEPROM(210));
+  //   Serial.println("   - estado = " + readStringFromEEPROM(220));
+  //   Serial.println("   - inactivity hours reset = " + readStringFromEEPROM(230));
+  //   Serial.println("   - set data realtime segundos = " + readStringFromEEPROM(240));
+  //   Serial.println("   - logger interval minutes = " + readStringFromEEPROM(250));
+  // }
 }
 
 ////// SETEO INICIAL DE DATOS //////////
 void jsonConfigDataSetFirstStart()
 {
   // SETEO DE CONFIGURACIÓN DE TARJET
-  jsonConfigTarjet.add("abcd", abcd);
-  jsonConfigTarjet.add("aforo", aforo_init);
-  jsonConfigTarjet.add("count-delay-milisegundos", count_dalay_milisegundos); // falta configurar
-  jsonConfigTarjet.add("estado", estado);                                     // falta configurar
-  jsonConfigTarjet.add("inactivity-hours-reset", inactivity_hours_reset);
-  jsonConfigTarjet.add("set-data-realtime-segundos", set_data_realtime_segundos); // falta configurar
-  jsonConfigTarjet.add("logger-interval-minutes", logger_interval_minutes);
+  jsonConfigTarjet.add("accidentes", 0);
+  // jsonConfigTarjet.add("aforo", aforo_init);
+  // jsonConfigTarjet.add("count-delay-milisegundos", count_dalay_milisegundos); // falta configurar
+  // jsonConfigTarjet.add("estado", estado);                                     // falta configurar
+  // jsonConfigTarjet.add("inactivity-hours-reset", inactivity_hours_reset);
+  // jsonConfigTarjet.add("set-data-realtime-segundos", set_data_realtime_segundos); // falta configurar
+  // jsonConfigTarjet.add("logger-interval-minutes", logger_interval_minutes);
 
-  // SETEO DE CONFIGURACIÓN WIFI
-  jsonConfigWifi.add("ssid", ssid);
-  jsonConfigWifi.add("password", password);
-  jsonConfigWifi.add("MAC-address", String(WiFi.macAddress())); // dirección mac
-  jsonConfigWifi.add("IP-address", String(String(WiFi.localIP()[0]) + "." + String(WiFi.localIP()[1]) + "." + String(WiFi.localIP()[2]) + "." + String(WiFi.localIP()[3])));
+  // // SETEO DE CONFIGURACIÓN WIFI
+  // jsonConfigWifi.add("ssid", ssid);
+  // jsonConfigWifi.add("password", password);
+  // jsonConfigWifi.add("MAC-address", String(WiFi.macAddress())); // dirección mac
+  // jsonConfigWifi.add("IP-address", String(String(WiFi.localIP()[0]) + "." + String(WiFi.localIP()[1]) + "." + String(WiFi.localIP()[2]) + "." + String(WiFi.localIP()[3])));
 
-  // SETEO DE CONFIGURACIÓN UDP
-  jsonConfigUdp.add("master-port", master_port);
-  jsonConfigUdp.add("second-port", second_port);
+  // // SETEO DE CONFIGURACIÓN UDP
+  // jsonConfigUdp.add("master-port", master_port);
+  // jsonConfigUdp.add("second-port", second_port);
 
-  // SETEO DE CONFIGURACIÓN DATA - REALTIME
-  jsonData.add("egresos", BDatos.egresos);
-  jsonData.add("excesos", BDatos.excesos);
-  jsonData.add("ingresos", BDatos.ingresos);
-  jsonData.add("total", BDatos.total);
+  // // SETEO DE CONFIGURACIÓN DATA - REALTIME
+  // jsonData.add("egresos", BDatos.egresos);
+  // jsonData.add("excesos", BDatos.excesos);
+  // jsonData.add("ingresos", BDatos.ingresos);
+  // jsonData.add("total", BDatos.total);
 
-  if (Firebase.RTDB.setJSONAsync(&fbdo, path_config + "/wifi", &jsonConfigWifi))
-  {
-    Serial.println();
-    Serial.println("Datos WIFI Firebase Realtime establecidos !!");
-  }
-  else
-  {
-    Serial.println();
-    Serial.println("*** Datos WIFI no establecidos en Firebase");
-  }
+  // if (Firebase.RTDB.setJSONAsync(&fbdo, path_config + "/wifi", &jsonConfigWifi))
+  // {
+  //   Serial.println();
+  //   Serial.println("Datos WIFI Firebase Realtime establecidos !!");
+  // }
+  // else
+  // {
+  //   Serial.println();
+  //   Serial.println("*** Datos WIFI no establecidos en Firebase");
+  // }
 
   if (Firebase.RTDB.setJSONAsync(&fbdo, path_config + "/tarjet", &jsonConfigTarjet))
   {
@@ -267,16 +267,16 @@ void jsonConfigDataSetFirstStart()
     Serial.println("*** Datos TARJET no establecidos en Firebase");
   }
 
-  if (Firebase.RTDB.setJSONAsync(&fbdo, path_config + "/udp", &jsonConfigUdp))
-  {
-    Serial.println("Datos UDP Firebase Realtime establecidos !!");
-  }
-  else
-  {
-    Serial.println("*** Datos UDP no establecidos en Firebase");
-  }
+  // if (Firebase.RTDB.setJSONAsync(&fbdo, path_config + "/udp", &jsonConfigUdp))
+  // {
+  //   Serial.println("Datos UDP Firebase Realtime establecidos !!");
+  // }
+  // else
+  // {
+  //   Serial.println("*** Datos UDP no establecidos en Firebase");
+  // }
 
-  Serial.printf("Set jsonData... %s\n", Firebase.RTDB.setJSONAsync(&fbdo, path_data, &jsonData) ? "ok" : fbdo.errorReason().c_str());
+  // Serial.printf("Set jsonData... %s\n", Firebase.RTDB.setJSONAsync(&fbdo, path_data, &jsonData) ? "ok" : fbdo.errorReason().c_str());
 }
 
 void jsonConfigDataSet()
@@ -284,39 +284,39 @@ void jsonConfigDataSet()
 
   // SETEO DE CONFIGURACIÓN DE TARJET
   jsonConfigTarjet.add("abcd", readStringFromEEPROM(200));
-  jsonConfigTarjet.add("aforo", readStringFromEEPROM(190).toInt());
-  jsonConfigTarjet.add("count-delay-milisegundos", readStringFromEEPROM(210).toInt()); // falta configurar
-  jsonConfigTarjet.add("estado", readStringFromEEPROM(220));                           // falta configurar
-  jsonConfigTarjet.add("inactivity-hours-reset", readStringFromEEPROM(230).toFloat());
-  jsonConfigTarjet.add("set-data-realtime-segundos", readStringFromEEPROM(240).toInt()); // falta configurar
-  jsonConfigTarjet.add("logger-interval-minutes", readStringFromEEPROM(250).toInt());
+  // jsonConfigTarjet.add("aforo", readStringFromEEPROM(190).toInt());
+  // jsonConfigTarjet.add("count-delay-milisegundos", readStringFromEEPROM(210).toInt()); // falta configurar
+  // jsonConfigTarjet.add("estado", readStringFromEEPROM(220));                           // falta configurar
+  // jsonConfigTarjet.add("inactivity-hours-reset", readStringFromEEPROM(230).toFloat());
+  // jsonConfigTarjet.add("set-data-realtime-segundos", readStringFromEEPROM(240).toInt()); // falta configurar
+  // jsonConfigTarjet.add("logger-interval-minutes", readStringFromEEPROM(250).toInt());
 
   // SETEO DE CONFIGURACIÓN WIFI
-  jsonConfigWifi.add("ssid", readStringFromEEPROM(0));
-  jsonConfigWifi.add("password", readStringFromEEPROM(50));
-  jsonConfigWifi.add("MAC-address", String(WiFi.macAddress())); // dirección mac
-  jsonConfigWifi.add("IP-address", String(String(WiFi.localIP()[0]) + "." + String(WiFi.localIP()[1]) + "." + String(WiFi.localIP()[2]) + "." + String(WiFi.localIP()[3])));
+  // jsonConfigWifi.add("ssid", readStringFromEEPROM(0));
+  // jsonConfigWifi.add("password", readStringFromEEPROM(50));
+  // jsonConfigWifi.add("MAC-address", String(WiFi.macAddress())); // dirección mac
+  // jsonConfigWifi.add("IP-address", String(String(WiFi.localIP()[0]) + "." + String(WiFi.localIP()[1]) + "." + String(WiFi.localIP()[2]) + "." + String(WiFi.localIP()[3])));
 
-  // SETEO DE CONFIGURACIÓN UDP
-  jsonConfigUdp.add("master-port", readStringFromEEPROM(150).toInt());
-  jsonConfigUdp.add("second-port", readStringFromEEPROM(160).toInt());
+  // // SETEO DE CONFIGURACIÓN UDP
+  // jsonConfigUdp.add("master-port", readStringFromEEPROM(150).toInt());
+  // jsonConfigUdp.add("second-port", readStringFromEEPROM(160).toInt());
 
-  // SETEO DE CONFIGURACIÓN DATA - REALTIME
-  jsonData.add("egresos", BDatos.egresos);
-  jsonData.add("excesos", BDatos.excesos);
-  jsonData.add("ingresos", BDatos.ingresos);
-  jsonData.add("total", BDatos.total);
+  // // SETEO DE CONFIGURACIÓN DATA - REALTIME
+  // jsonData.add("egresos", BDatos.egresos);
+  // jsonData.add("excesos", BDatos.excesos);
+  // jsonData.add("ingresos", BDatos.ingresos);
+  // jsonData.add("total", BDatos.total);
 
-  if (Firebase.RTDB.setJSONAsync(&fbdo, path_config + "/wifi", &jsonConfigWifi))
-  {
-    Serial.println();
-    Serial.println("Datos WIFI Firebase Realtime establecidos !!");
-  }
-  else
-  {
-    Serial.println();
-    Serial.println("*** Datos WIFI no establecidos en Firebase");
-  }
+  // if (Firebase.RTDB.setJSONAsync(&fbdo, path_config + "/wifi", &jsonConfigWifi))
+  // {
+  //   Serial.println();
+  //   Serial.println("Datos WIFI Firebase Realtime establecidos !!");
+  // }
+  // else
+  // {
+  //   Serial.println();
+  //   Serial.println("*** Datos WIFI no establecidos en Firebase");
+  // }
 
   if (Firebase.RTDB.setJSONAsync(&fbdo, path_config + "/tarjet", &jsonConfigTarjet))
   {
@@ -327,16 +327,16 @@ void jsonConfigDataSet()
     Serial.println("*** Datos TARJET no establecidos en Firebase");
   }
 
-  if (Firebase.RTDB.setJSONAsync(&fbdo, path_config + "/udp", &jsonConfigUdp))
-  {
-    Serial.println("Datos UDP Firebase Realtime establecidos !!");
-  }
-  else
-  {
-    Serial.println("*** Datos UDP no establecidos en Firebase");
-  }
+  // if (Firebase.RTDB.setJSONAsync(&fbdo, path_config + "/udp", &jsonConfigUdp))
+  // {
+  //   Serial.println("Datos UDP Firebase Realtime establecidos !!");
+  // }
+  // else
+  // {
+  //   Serial.println("*** Datos UDP no establecidos en Firebase");
+  // }
 
-  Serial.printf("Set jsonData... %s\n", Firebase.RTDB.setJSONAsync(&fbdo, path_data, &jsonData) ? "ok" : fbdo.errorReason().c_str());
+  // Serial.printf("Set jsonData... %s\n", Firebase.RTDB.setJSONAsync(&fbdo, path_data, &jsonData) ? "ok" : fbdo.errorReason().c_str());
 }
 
 void actualizarConfigFlash()
@@ -345,8 +345,8 @@ void actualizarConfigFlash()
   Serial.println("<<<<<   A C T U A L I Z A N D O   M E M O R I A   F L A S H   <<<<<");
   setCofigEprom();                                  // actualizamos valores
   //jsonConfigDataSet();                              // enviamos actualización
-  BDatos.aforo = readStringFromEEPROM(190).toInt(); // siempre actualizamos el aforo
-  horas_inactividad_max = readStringFromEEPROM(230).toFloat();
-  localPort = readStringFromEEPROM(150).toInt();
-  remotePort = readStringFromEEPROM(160).toInt();
+  // BDatos.aforo = readStringFromEEPROM(190).toInt(); // siempre actualizamos el aforo
+  // horas_inactividad_max = readStringFromEEPROM(230).toFloat();
+  // localPort = readStringFromEEPROM(150).toInt();
+  // remotePort = readStringFromEEPROM(160).toInt();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,9 @@
 #include <Firebase_ESP_Client.h> //#include <Firebase_ESP_Client.h>
 #include <EEPROM.h>              // update de memorias fash
 #include <Separador.h>
+#include <EEPROM.h>              // update de memorias fash
 
-
+Separador s; // instancia para separar string
 
 // NTP Servers:
 WiFiUDP ntpUDP;
@@ -19,7 +20,9 @@ String formattedDate;
 String dayStamp;
 String timeStamp;
 
-
+int yearNtp = 0;
+int montNtp = 0;
+int dayNtp = 0;
 
 // Define Firebase Data object
 FirebaseData fbdo;
@@ -31,7 +34,6 @@ FirebaseJson jsonConfigWifi;
 FirebaseJson jsonConfigUdp;
 FirebaseJson jsonConfigTarjet;
 FirebaseJson jsonData;
-
 
 #include "config.h" // Configurar datos de la red
 #include "ESP8266_Utils.hpp"
@@ -45,47 +47,118 @@ FirebaseJson jsonData;
 #include <addons/RTDBHelper.h>
 
 /* 2. Define the API Key */
-#define API_KEY "AIzaSyACwNdd8nTeRCLSr6tFyZLQ-jquw8ljKa8"
+#define API_KEY "AIzaSyC1quVUqw2XhlUumh6cbXHwlTx0KSSRapI"
 
 /* 3. Define the RTDB URL */
-#define DATABASE_URL "prueba-2-e4543-default-rtdb.firebaseio.com" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+#define DATABASE_URL "display-accidentes-komatsu-default-rtdb.firebaseio.com" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
 
 /* 3. Define the project ID */
-#define FIREBASE_PROJECT_ID "prueba-2-e4543"
+#define FIREBASE_PROJECT_ID "display-accidentes-komatsu"
 
 /* 4. Define the user Email and password that alreadey registerd or added in your project */
-#define USER_EMAIL "comedor1@tasa-callao.com"
-#define USER_PASSWORD "comedor1"
+#define USER_EMAIL "display1@komatsu.com"
+#define USER_PASSWORD "komatsu"
 
+/////////// ROUTINE PARA ACCTUALIZAR HORA ////////////
+unsigned long int prevMillis = 0;
+int splitT = 0;
+void ntpTimeUpdate()
+{
+  if (millis() < 500)
+    prevMillis = 0; // condicional de protección al desbordamiento
 
+  if ((millis() - prevMillis) > 1000)   // actualizamos he imprimimos cada 1s
+  { // actualización time cada 1s
+    prevMillis = millis();
+    if (!timeClient.update())
+    {
+      timeClient.forceUpdate();
+    }
+
+    // The formattedDate comes with the following format:
+    // 2018-05-28T16:00:13Z
+    // We need to extract date and time
+    formattedDate = timeClient.getFormattedDate();  // example 2018-05-28T16:00:13Z
+
+    // Extract date
+    splitT = formattedDate.indexOf("T");
+    dayStamp = formattedDate.substring(0, splitT) + '-';  // example 2018-05-28-
+    yearNtp = s.separa(dayStamp,'-',0 ).toInt();
+    montNtp = s.separa(dayStamp,'-',1 ).toInt();
+    dayNtp = s.separa(dayStamp, '-',2).toInt();
+
+    // Serial.println(dayStamp);
+    // Serial.println("Y:" + String(yearNtp) + " M:" + String(montNtp) + " D:" + String(dayNtp));
+    Serial.println('/'+ String(montNtp) + '/' + readStringFromEEPROM(0));   // enviar data a display
+
+    // Extract time
+    // timeStamp = formattedDate.substring(splitT + 1, formattedDate.length() - 1);
+    // Serial.print("HOUR: ");
+    // Serial.println(timeStamp);
+  }
+}
 
 void setup()
 {
   Serial.begin(9600);
+  EEPROM.begin(260);
   ConnectWiFi_STA();
   timeClient.begin();
   timeClient.setTimeOffset(-18000); // -5 * 3600
+
+  // -------------- FIREBASE -------------------------
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+  // Or use legacy authenticate method
+  // config.database_url = DATABASE_URL;
+  // config.signer.tokens.legacy_token = "<database secret>";
+
+  Firebase.begin(&config, &auth);
+
+  // Comment or pass false value when WiFi reconnection will control by your code or third party library
+  Firebase.reconnectWiFi(true);
+
+  // Optional, use classic HTTP GET and POST requests.
+  // This option allows get and delete functions (PUT and DELETE HTTP requests) works for
+  // device connected behind the Firewall that allows only GET and POST requests.
+  Firebase.RTDB.enableClassicRequest(&fbdo, true);
+
+  // ------ EPROM INIT ----
+  // condicional puesto para validar que los datos esten escritos en firebase
+  if (Firebase.ready())
+  {
+    if (String("null") == String(Firebase.RTDB.getString(&fbdo, path_config + "/" + "wifi" + "/" + "ssid" + "/") ? fbdo.to<const char *>() : fbdo.errorReason().c_str()))
+    {
+      Serial.println(">>> Estableciendo datos por defecto en Firebase");
+      jsonConfigDataSetFirstStart(); // si  no estan escritos primero crea los datos en la nube
+      Serial.println("<<< Grabando datos en memoria");
+      setCofigEprom(); // luego los lee y guarda en eeprom
+    }
+    else
+    {
+      Serial.println("<<< Lectura de Data en Firebase y guardado en la EEPROM");
+      setCofigEprom();     // si ya existen los datos, primero lee y guarda el la eeprom
+      jsonConfigDataSet(); // escribe los datos los nuevos datos en la nuebe
+    }
+  }
 }
-int splitT = 0;
+
+
 void loop()
 {
-  if(!timeClient.update()) {
-    timeClient.forceUpdate();
-  }
-  // The formattedDate comes with the following format:
-  // 2018-05-28T16:00:13Z
-  // We need to extract date and time
-  formattedDate = timeClient.getFormattedDate();
-  Serial.println(formattedDate);
-
-  // Extract date
-  splitT = formattedDate.indexOf("T");
-  dayStamp = formattedDate.substring(0, splitT);
-  Serial.print("DATE: ");
-  Serial.println(dayStamp);
-  // Extract time
-  timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
-  Serial.print("HOUR: ");
-  Serial.println(timeStamp);
-  delay(1000);
+  ntpTimeUpdate();
+  actualizarConfigFlash();
 }
